da74c7a7448096cb4c2e9432813ae813f4e9f372
Feng <afeng@urban.org>
2020-06-23 13:48:07 -0400

    Begin setting up final HTML structure

--- js/drawCharts.js
+++ js/drawCharts.js
@@ -3,176 +3,67 @@ var pulseData;
 var h = 300,
     w = 320;
 
-function drawGraphic(containerWidth) {
-    var nest = d3.nest()
-        .key(function(d){ return d.metric })
-        .entries(pulseData)
-
-    var metric = d3.select("#chart_container")
-        .selectAll(".metric")
-        .data(nest)
-        .enter().append("div")
-        .attr("class", function(d){ return(d.key == "uninsured") ? "metric " + d.key : "metric hide " + d.key})
-    metric.append("div")
-        .attr("class", "metricName")
-        .text(function(d){ return d.key })
-
-
-    var city = metric
-        .selectAll(".city")
-        .data(function(n){   return d3.nest()
-            .key(function(o){ return o.geography })
-            .entries(n.values)
-        })
-        .enter().append("div")
-        .attr("class", function(d){
-          return (d.key == "US") ?  "city " + d.key.replace(/\W/g,'_') : "city hide " + d.key.replace(/\W/g,'_')
-        })
-
-    city.append("div")
-        .attr("class", "cityName")
-        .text(function(d){ return d.key })
-
-    // console.log(nest)
-
-    var race = city
-    .selectAll(".race")
-    .data(function(n){
-        var t = n.values.filter(function(r){ return r.race_var == "total"})
-        n.values.forEach(function(nv){
-            nv.total = t
-        })
-        // console.log(n.values)
-        return d3.nest()
-            .key(function(o){ return o.race_var })
-            .entries(n.values)
-
-    })
-    .enter().append("div")
-    .attr("class", function(d){ return "race " + d.key })
-
-    race.append("div")
-        .attr("class", "raceName")
-        .text(function(d){ return d.key })
-
-    var svg = race.append("svg").attr("width",w).attr("height",h)
-
-   var margin = {
+var margin = {
     top: 10,
     right: 10,
     bottom: 25,
     left: 25
-    },
+},
     width = w - margin.left - margin.right,
-    height = h - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    height = h - margin.top - margin.bottom;
 
+var x = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.5)
+    .domain(["wk1_2","wk2_3","wk3_4"]);
 
-    var x = d3.scaleBand()
-        .rangeRound([0, width])
-        .padding(0.5)
-        .domain(["wk1_2","wk2_3","wk3_4"])
+var y = d3.scaleLinear()
+    .domain([0,1])
+    .rangeRound([height, 0]);
 
-    var y = d3.scaleLinear()
-        .domain([0,1])
-        .rangeRound([height, 0]);
+function drawGraphic(containerWidth) {
+
+    var example_data = pulseData.filter(function(d) { return d.geography === "MD" && (d.race_var === "asian" || d.race_var === "total") && d.metric === "uninsured"; });
+console.log(example_data);
+    var svg = d3.select(".chart.asian svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    var g = svg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     g.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
     g.append("g")
-    .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y));
 
+    // draw margin of error bands
     g.selectAll(".moe")
-        .data(function(d){
-             return d.values
+        .data(example_data)
+        .enter()
+        .append("rect")
+        .attr("class", function(d) { return d.race_var === "total" ? "tmoe" : "moe"; })
+        .attr("x", function(d) { return x(d.week_num); })
+        .attr("y", function(d) { return y(+d.moe_95_ub); })
+        .attr("width", function(d) { return x.bandwidth(); })
+        .attr("height", function(d) {
+            return (+d.moe_95_lb < 0) ? y(0) -y(+d.moe_95_ub) : y(+d.moe_95_lb) - y(+d.moe_95_ub);
         })
-        .enter().append("rect")
-        .attr("class", "moe chartel")
-        .attr("x", function(d){
-            // console.log(d)
-            return x(d.week_num)
-        })
-        .attr("y", function(d){
-            return y(+d.moe_95_ub)
-        })
-        .attr("width", function(d){
-            return x.bandwidth()
-        })
-        .attr("height", function(d){
-            return (+d.moe_95_lb < 0) ? y(0) -y(+d.moe_95_ub) : y(+d.moe_95_lb) - y(+d.moe_95_ub)
-        })
-        .classed("insig", function(d){
-            return (d.sigdiff == "0")
-        })
+        .classed("insig", function(d) { return (d.sigdiff === 0); });
 
-
+    // draw point estimate dots
     g.selectAll(".dot")
-        .data(function(d){
-             return d.values
-        })
-        .enter().append("circle")
-        .attr("class", "dot")
-        .attr("cx", function(d){
-            return x(d.week_num) + x.bandwidth()*.5
-        })
-        .attr("cy", function(d){
-            return y(+d.mean)
-        })
+        .data(example_data)
+        .enter()
+        .append("circle")
+        .attr("class", function (d) { return d.race_var === "total" ? "tdot" : "dot"; })
+        .attr("cx", function(d) { return x(d.week_num) + x.bandwidth()*.5; })
+        .attr("cy", function(d) { return y(+d.mean); })
         .attr("r", 4)
-        .classed("insig", function(d){
-            return (d.sigdiff == "0")
-        })
-
-    var tg = g.selectAll(".tg")
-        .data(function(d){
-             return d.values
-        })
-        .enter().append("g")
-        .attr("clas","tg")
-
-    tg.selectAll(".tdot")
-        .data(function(d){
-            // console.log(d)
-            return d.total
-        })
-        .enter().append("circle")
-        .attr("class", "tdot chartel hide")
-        .attr("cx", function(d){
-            // console.log(d)
-            return x(d.week_num) + x.bandwidth()*.5
-        })
-        .attr("cy", function(d){
-            return y(+d.mean)
-        })
-        .attr("r", 4)
-        .classed("insig", function(d){
-            return (d.sigdiff == "0")
-        })
-
-    tg.selectAll(".tmoe")
-        .data(function(d){
-             return d.total
-        })
-        .enter().append("rect")
-        .attr("class", "tmoe hide chartel")
-        .attr("x", function(d){
-            // console.log(d)
-            return x(d.week_num)
-        })
-        .attr("y", function(d){
-            return y(+d.moe_95_ub)
-        })
-        .attr("width", function(d){
-            return x.bandwidth()
-        })
-        .attr("height", function(d){
-            return (+d.moe_95_lb < 0 ) ? y(0) -y(+d.moe_95_ub) : y(+d.moe_95_lb) - y(+d.moe_95_ub)
-        })
-        .classed("insig", function(d){
-            return (d.sigdiff == "0")
-        })
+        .classed("insig", function(d) { return (d.sigdiff === 0); });
 
     if (pymChild) {
         pymChild.sendHeight();
