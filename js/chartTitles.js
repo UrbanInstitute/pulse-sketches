277e54fd52e39adbbb107946a507d81d20ffbb19
Feng <afeng@urban.org>
2020-06-24 17:05:38 -0400

    Update with latest list of metrics

--- js/chartTitles.js
+++ js/chartTitles.js
@@ -3,7 +3,8 @@ var chartTitles = {
     insured_public: "Share of adults under 65 that have public insurance coverage",
     inc_loss: "Share of adults in households where at least one person has lost income since March 1st",
     expect_inc_loss: "Share of adults in households where at least one person expects to lose income in the next four weeks",
-    payment_not_conf: "",
+    rent_not_conf: "Share of adults in households with a rent unlikely to pay it next month",
+    mortgage_not_conf: "Share of adults in households with a mortgage unlikely to pay it next month",
     food_insufficient: "Share of adults in households where there is often or sometimes not enough food",
     classes_cancelled: "Share of adults in households with children enrolled in public or private school where at least one child had class cancelled because of COVID-19",
     depression_anxiety_signs: "Share of adults that experienced symptoms of depression or anxiety in the last week"
